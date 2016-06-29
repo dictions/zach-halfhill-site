e46d10648683224a5636084acaf266e61bfcc5e7
Ian Williams <me@ianwillia.ms>
2016-06-29 17:07:26 +0200

    Update 2016-06-29T15:07:14.376Z

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-var PRODUCTION = process.env.NODE_ENV === 'production';
-
-if (PRODUCTION) {
-	module.exports = require('./webpack/prod.config');
-} else {
-	module.exports = require('./webpack/dev.config');
-}
