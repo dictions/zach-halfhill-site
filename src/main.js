30f1f2b244860570a28cb136cc358125c8cc51b2
Ian Williams <me@ianwillia.ms>
2016-06-30 01:42:06 +0200

    Replace logo cycle w/ random logo.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
-import cycleLogos from './components/logo';
-
 // Compile styles
 import './styles/main.scss';
-
-// Cycle logos
-cycleLogos({
-	logoSelector: '.header__logotype-img',
-	interval: 1000
-});
