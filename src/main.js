d852f53fd1701eaee704b0338aa124f1916d425f
Ian Williams <me@ianwillia.ms>
2016-06-30 02:28:38 +0200

    Prevent defaults when refreshing.

--- src/main.js
+++ src/main.js
@@ -1,2 +1,36 @@
 // Compile styles
 import './styles/main.scss';
+
+// Pick a random logo
+const placeholder = document.querySelector('.header__logotype-placeholder');
+const logos = document.querySelectorAll('.header__logotype-img');
+let index = Math.floor(Math.random() * logos.length);
+let previousIndex;
+
+// Try to load previous logo to prevent defaults
+try {
+	previousIndex = Number(localStorage.getItem('previousIndex'));
+} catch (e) {
+	throw new Error(e);
+}
+
+// Pick a different logo if duplicate found
+while (index === previousIndex) {
+	index = Math.floor(Math.random() * logos.length);
+}
+
+// Set previous logo to prevent defaults
+try {
+	localStorage.setItem('previousIndex', index);
+} catch (e) {
+	throw new Error(e);
+}
+
+const currentLogo = logos[index];
+
+currentLogo.onload = () => {
+	currentLogo.classList.add('visible');
+	placeholder.classList.remove('visible');
+};
+
+currentLogo.src = currentLogo.dataset.src;
